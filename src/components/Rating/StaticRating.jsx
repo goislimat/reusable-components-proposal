ddf69a85f7ba882cfd7ecf9e7ae8ac4a37c92cd2
Thiago Gois <goislimat2@gmail.com>
2023-05-10 14:31:23 -0300

    This commit will remove unnecessary rating style file

--- src/components/Rating/StaticRating.jsx
+++ src/components/Rating/StaticRating.jsx
@@ -1,16 +1,13 @@
 import PropTypes from "prop-types";
 
-import { Wrapper } from "./StaticRating.style";
 import * as Rating from "./components";
 
 function StaticRating({ value, max }) {
   return (
     <Rating.Root rating={value} max={max}>
-      <Wrapper>
-        <Rating.Container>
-          {({ isHighlighted }) => <Rating.Icon isHighlighted={isHighlighted} />}
-        </Rating.Container>
-      </Wrapper>
+      <Rating.Container>
+        {({ isHighlighted }) => <Rating.Icon isHighlighted={isHighlighted} />}
+      </Rating.Container>
     </Rating.Root>
   );
 }
